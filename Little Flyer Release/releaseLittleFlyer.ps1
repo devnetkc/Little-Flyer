90faa0435ba25f682419a00629de968e60ef0a38
Ryan Valizan <r.valizan@cfmkc.com>
2018-04-30 12:17:14 -0500

    lf-prep_script

--- Little Flyer Release/releaseLittleFlyer.ps1
+++ Little Flyer Release/releaseLittleFlyer.ps1
@@ -2,3 +2,12 @@
 # Developer: Ryan Valizan
 # Project: Little Flyer Automated Release
 #
+
+$curYr = Get-Date -format YYYY
+$curMon = Get-Date -format MMM
+$saveLoc = "C:\Users\ryan\OneDrive - cfm Distributors, Inc\_Website\Wordpress\cfm_live\images\Little Flyer\" + $curYr
+$workDir = "$($saveLoc)\workDir"
+$newDir = $saveLoc+$curMon
+
+Copy-Item "$($saveLoc)\*" $newDir
+dir $saveLoc | rename-item -NewName {$_.name -replace "$$$",$curMon}
