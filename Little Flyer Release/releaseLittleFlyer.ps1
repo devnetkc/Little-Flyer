4692441f45e69f83084b2c7ce2c5295806162d06
Ryan Valizan <r.valizan@cfmkc.com>
2018-04-30 12:23:41 -0500

    adding error checking to variable setting.

--- Little Flyer Release/releaseLittleFlyer.ps1
+++ Little Flyer Release/releaseLittleFlyer.ps1
@@ -1,15 +1,33 @@
 ﻿#
 # Developer: Ryan Valizan
 # Project: Little Flyer Automated Release
+# Purpose: Rename all the Little Flyer images and put them in their folder to upload to the server.
 #
 
-$gDate = Get-Date
-$cYR = $gDate.ToString('yyyy')
-$cMON = $gDate.ToString('MMM')
-$saveLoc = 'C:\Users\ryan\OneDrive - cfm Distributors, Inc\_Website\Wordpress\cfm_live\images\Little Flyer\' + $cYR
-$workDir = $saveLoc+'\workDir'
-$newDir = $saveLoc + '\' + $cMON
-$copyFrom = $workDir + '\*' 
+# clear our error count
+if ($error.Count -gt 0) {
+    $error.clear()
+}
+
+# try setting up our date variables
+try {
+    $a = new-object -comobject wscript.shell 
+    $curMonthAbv = ((Get-Date).AddMonths(0)).ToString('MMM')
+    $nextMonthAbv = ((Get-Date).AddMonths(1)).ToString('MMM')
+    $nextMonth = ((Get-Date).AddMonths(1)).ToString('MMMM')
+
+	$gDate = Get-Date
+	$cYR = $gDate.ToString('yyyy')
+	$cMON = $gDate.ToString('MMM')
+	$saveLoc = 'C:\Users\ryan\OneDrive - cfm Distributors, Inc\_Website\Wordpress\cfm_live\images\Little Flyer\' + $cYR
+	$workDir = $saveLoc+'\workDir'
+	$newDir = $saveLoc + '\' + $cMON
+	$copyFrom = $workDir + '\*' 
+}
+catch {
+    myErrorMessage(17)
+}
+
 If(!(test-path $newDir))
     {
         New-Item -ItemType directory -Force -Path $newDir
